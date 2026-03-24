2ed96f319461f4d126b39cee5eeae072cd8414e1
soryedouardodiassana7-png <soryedouardodiassana7@gmail.com>
2026-03-24 20:46:10 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from flask import Flask
+import threading
+import os
 
 TOKEN = "8746516306:AAEse0EuPp7_8pI_dQ1e6lWjWsxdbj-yTjg"
+app_web = Flask(__name__)
+
+@app_web.route('/')
+def home():
+    return "Bot actif"
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    app_web.run(host="0.0.0.0", port=port)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Galaxy Rename Bot active 🚀")
 
-app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(CommandHandler("start", start))
+bot = ApplicationBuilder().token(TOKEN).build()
+bot.add_handler(CommandHandler("start", start))
 
-app.run_polling()
+threading.Thread(target=run_web).start()
+
+bot.run_polling()
