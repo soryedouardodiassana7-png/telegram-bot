62985a6a82ad139433043df25795da2175ee9824
soryedouardodiassana7-png <soryedouardodiassana7@gmail.com>
2026-03-24 19:23:41 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-8746516306:AAHmQ9TYJE-hfJYRXdnXWRJ6zS1EXtVvb6s
-
+TOKEN = 8746516306:AAHmQ9TYJE-hfJYRXdnXWRJ6zS1EXtVvb6s
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 await update.message.reply_text("Galaxy Rename Bot active 🚀")
 
